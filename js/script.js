81c45775c5aeefab15fe4a283015492051755a06
Vaclav Nekolny <vaclav.nekolny50@gmail.com>
2025-03-20 11:46:16 +0100

    navbar dynamický; collapse se zavře při kliknutí na link

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function userScroll() {
+  const navbar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("navbar-shrink");
+    } else {
+      navbar.classList.remove("navbar-shrink");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", userScroll);
+
+// Zavření collapse menu po kliknutí na odkaz .nav-link
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarCollapse = document.querySelector(".navbar-collapse");
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      if (navbarCollapse.classList.contains("show")) {
+        new bootstrap.Collapse(navbarCollapse, { toggle: true });
+      }
+    });
+  });
+});
