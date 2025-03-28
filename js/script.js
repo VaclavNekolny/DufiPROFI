72f6a4a0cb21a92fcb123ccc85de74a4c52e9a40
Vaclav Nekolny <vaclav.nekolny50@gmail.com>
2025-03-28 22:39:51 +0100

    products - reálné fotky, úprava zobrazení

--- js/script.js
+++ js/script.js
@@ -12,10 +12,11 @@ function userScroll() {
 
 document.addEventListener("DOMContentLoaded", userScroll);
 
-// Zavření collapse menu po kliknutí na odkaz .nav-link
+// Zavření collapse menu po kliknutí na odkaz .nav-link a .navbar-brand
 document.addEventListener("DOMContentLoaded", function () {
   const navbarCollapse = document.querySelector(".navbar-collapse");
   const navLinks = document.querySelectorAll(".nav-link");
+  const navbarBrand = document.querySelector(".navbar-brand");
 
   navLinks.forEach(function (link) {
     link.addEventListener("click", function () {
@@ -23,5 +24,11 @@ document.addEventListener("DOMContentLoaded", function () {
         new bootstrap.Collapse(navbarCollapse, { toggle: true });
       }
     });
+  });
+
+  navbarBrand.addEventListener("click", function () {
+    if (navbarCollapse.classList.contains("show")) {
+      new bootstrap.Collapse(navbarCollapse, { toggle: true });
+    }
   });
 });
