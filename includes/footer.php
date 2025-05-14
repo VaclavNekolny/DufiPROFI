58e609f02b7205522c2707ef650803e96accb832
Vaclav Nekolny <vaclav.nekolny50@gmail.com>
2025-05-14 22:30:39 +0200

    Ceník modal - úprava a prolinkování

--- includes/footer.php
+++ includes/footer.php
@@ -1,64 +1,56 @@
 <footer class="footer bg-dark text-primary py-4">
-      <div class="container">
-        <div class="row g-3">
-          <div class="col-lg-3 offset-lg-2 col-md-4 offset-md-1">
-            <h5>Kontakt</h5>
-            <ul class="list-unstyled">
-              <li>Veronika Dufková</li>
-              <li>Jirkov, 431 11</li>
-              <li>
-                Mobil:
-                <a class="text-decoration-none" href="tel:+420776575581"
-                  >+420 776 575 581</a
-                >
-              </li>
-              <li>
-                E-mail:
-                <a
-                  class="text-decoration-none"
-                  href="mailto:dufiprofi@seznam.cz"
-                  >dufiprofi@seznam.cz</a
-                >
-              </li>
-              <li>IČ: 14327228</li>
-            </ul>
-          </div>
+  <div class="container">
+    <div class="row g-3">
+      <div class="col-lg-3 offset-lg-2 col-md-4 offset-md-1">
+        <h5>Kontakt</h5>
+        <ul class="list-unstyled">
+          <li>Veronika Dufková</li>
+          <li>Jirkov, 431 11</li>
+          <li>
+            Mobil:
+            <a class="text-decoration-none" href="tel:+420776575581">+420 776 575 581</a>
+          </li>
+          <li>
+            E-mail:
+            <a
+              class="text-decoration-none"
+              href="mailto:dufiprofi@seznam.cz">dufiprofi@seznam.cz</a>
+          </li>
+          <li>IČ: 14327228</li>
+        </ul>
+      </div>
 
-          <div class="col-lg-2 col-md-3">
-            <h5>Rychlé&nbsp;odkazy</h5>
-            <ul class="list-unstyled">
-              <li><a href="index.html#home">Domů</a></li>
-              <li><a href="index.html#sluzby">Služby</a></li>
-              <li><a href="index.html#o_me">O mě</a></li>
-              <li><a href="fotogalerie.html">Portfolio</a></li>
-              <li><a href="index.html#kontakt">Kontakt</a></li>
-            </ul>
-          </div>
+      <div class="col-lg-2 col-md-3">
+        <h5>Rychlé&nbsp;odkazy</h5>
+        <ul class="list-unstyled">
+          <li><a href="index.html#home">Domů</a></li>
+          <li><a href="index.html#sluzby">Služby</a></li>
+          <li><a href="index.html#o_me">O mě</a></li>
+          <li><a href="fotogalerie.html">Portfolio</a></li>
+          <li><a href="index.html#kontakt">Kontakt</a></li>
+        </ul>
+      </div>
 
-          <div class="col-lg-3 col-md-4">
-            <div
-              class="d-flex flex-column gap-4 justify-content-center align-items-center"
-            >
-              <h5>Sledujte&nbsp;mě</h5>
-              <div>
-                <a
-                  href="https://www.facebook.com/profile.php?id=100083148499662"
-                  target="new"
-                  class="me-3"
-                  ><i class="fab fa-facebook fa-3x"></i
-                ></a>
-                <a href="https://www.instagram.com/dufiprofi/" target="new">
-                  <i class="fab fa-instagram fa-3x"></i
-                ></a>
-              </div>
-              <div class="mt-2">
-                <small>&copy; 2025 | <a href="#">Václav Nekolný</a></small>
-              </div>
-            </div>
+      <div class="col-lg-3 col-md-4">
+        <div
+          class="d-flex flex-column gap-4 justify-content-center align-items-center">
+          <h5>Sledujte&nbsp;mě</h5>
+          <div>
+            <a
+              href="https://www.facebook.com/profile.php?id=100083148499662"
+              target="new"
+              class="me-3"><i class="fab fa-facebook fa-3x"></i></a>
+            <a href="https://www.instagram.com/dufiprofi/" target="new">
+              <i class="fab fa-instagram fa-3x"></i></a>
+          </div>
+          <div class="mt-2">
+            <small>&copy; 2025 | <a href="#">Václav Nekolný</a></small>
           </div>
         </div>
       </div>
-    </footer>
+    </div>
+  </div>
+</footer>
 
 
 <!-- CENÍK modal-->
@@ -67,69 +59,63 @@
   id="cenikModal"
   tabindex="-1"
   aria-labelledby="cenikModalLabel"
-  aria-hidden="true"
->
+  aria-hidden="true">
   <div
-    class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable"
-  >
+    class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
     <div class="modal-content">
-      <div class="modal-header bg-primary">
+      <div class="modal-header bg-primary p-4">
         <h5 class="modal-title" id="cenikModalLabel">Ceník služeb</h5>
         <button
           type="button"
           class="btn-close"
           data-bs-dismiss="modal"
-          aria-label="Zavřít"
-        ></button>
+          aria-label="Zavřít"></button>
       </div>
-      <div class="modal-body bg-primary">
+      <div class="modal-body bg-primary p-4">
         <h6 class="mt-3">JEDNOTLIVÉ SLUŽBY</h6>
         <hr />
         <strong>NĚVĚSTA</strong><br />
         Nevěsta zkouška líčení a účesu
-        <span class="float-end">2 000 Kč</span><br />
-        Nevěsta ve svatební den <span class="float-end">4 000 Kč</span><br />
-        Svatebčanky <span class="float-end">1 000 – 2 000 Kč</span><br /><br />
+        <span class="float-end fw-bold">2 000 Kč</span><br />
+        Nevěsta ve svatební den <span class="float-end fw-bold">4 000 Kč</span><br />
+        Svatebčanky <span class="float-end fw-bold">1 000 – 2 000 Kč</span><br /><br />
 
         <strong>MATURANTKA</strong><br />
         Maturantka zkouška líčení a účesu
-        <span class="float-end">1 500 Kč</span><br />
+        <span class="float-end fw-bold">1 500 Kč</span><br />
         Maturantka v den plesu
-        <span class="float-end">2 000 – 3 000 Kč</span><br /><br />
+        <span class="float-end fw-bold">2 000 – 3 000 Kč</span><br /><br />
 
         <strong>SPOLEČENSKÁ UDÁLOST</strong><br />
         (plesy, večírky, narozeniny, focení)<br />
-        Líčení a účes <span class="float-end">2 000 – 3 000 Kč</span><br />
-        Líčení samostatně <span class="float-end">1 000 - 1 500 Kč</span><br />
-        Účes samostatně <span class="float-end">1 000 - 1 500 Kč</span><br />
-        Zkouška <span class="float-end">2 000 Kč</span><br /><br />
+        Líčení a účes <span class="float-end fw-bold">2 000 – 3 000 Kč</span><br />
+        Líčení samostatně <span class="float-end fw-bold">1 000 - 1 500 Kč</span><br />
+        Účes samostatně <span class="float-end fw-bold">1 000 - 1 500 Kč</span><br />
+        Zkouška <span class="float-end fw-bold">2 000 Kč</span><br /><br />
 
         <h6 class="mt-4">DALŠÍ INFORMACE</h6>
         <hr />
-        <strong>CESTOVNÉ</strong> <span class="float-end">10 Kč/km</span><br />
-        <small
-          >Cestovné je počítáno tam i zpět. Do cestovného je zahrnuto i
+        <strong>CESTOVNÉ</strong> <span class="float-end fw-bold">10 Kč/km</span><br />
+        <small>Cestovné je počítáno tam i zpět. Do cestovného je zahrnuto i
           parkovné.<br />
-          Jezdím po celém Ústeckém kraji, po domluvě i mimo.</small
-        ><br /><br />
+          Jezdím po celém Ústeckém kraji, po domluvě i mimo.</small><br /><br />
 
         <strong>REZERVAČNÍ POPLATEK</strong>
-        <span class="float-end">1 000 Kč</span><br />
-        <small
-          >Rezervační poplatek je nevratný. Tento poplatek se odečítá z finální
-          částky za služby.</small
-        ><br /><br />
+        <span class="float-end fw-bold">1 000 Kč</span><br />
+        <small>Rezervační poplatek je nevratný. Tento poplatek se odečítá z finální
+          částky za služby.</small><br /><br />
 
         <strong>CENY SLUŽEB</strong><br />
         <small>Odvíjejí se od náročnosti a spotřebovaného materiálu.</small>
       </div>
       <div class="modal-footer bg-primary justify-content-center">
-        <button type="button" class="btn btn-outline-dark">Služby</button>
+        <a href="?page=cenik" class="btn btn-outline-dark">Detail služeb</a>
       </div>
     </div>
   </div>
 </div>
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/script.js"></script>
-  </body>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+
 </html>
